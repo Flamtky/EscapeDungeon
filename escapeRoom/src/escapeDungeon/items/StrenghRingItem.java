36153f0176fdb63bed075a8adf94ab3bc1c0a4d8
b-franklin <benjamin.franke@fh-bielefeld.de>
2026-01-10 16:37:38 +0100

    add items

---
+++ escapeRoom/src/escapeDungeon/items/StrenghRingItem.java
@@ -0,0 +1,58 @@
+package escapeDungeon.items;
+
+import contrib.components.SkillComponent;
+import contrib.item.Item;
+import contrib.utils.components.skill.Resource;
+import core.Entity;
+import core.utils.Point;
+import core.utils.Tuple;
+import core.utils.components.draw.animation.Animation;
+import core.utils.components.path.SimpleIPath;
+import escapeDungeon.components.IceMovementComponent;
+import escapeDungeon.skill.IceWallSkill;
+import mushRoom.Sounds;
+
+import java.util.Optional;
+
+public class StrenghRingItem extends Item {
+
+  private static final String PATH = "items/rpg/item_ring_gold_gem_red.png";
+
+  private static Entity itemHolder;
+
+  public StrenghRingItem() {
+    super(
+        "ein magischer Eisring",
+        "",
+        new Animation(new SimpleIPath(PATH)),
+        new Animation(new SimpleIPath(PATH)));
+  }
+
+  @Override
+  public void use(Entity user) {}
+
+  @Override
+  public boolean collect(Entity itemEntity, Entity collector) {
+    itemHolder = collector;
+    Sounds.KEY_ITEM_PICKUP_SOUND.play();
+    collector.add(new IceMovementComponent());
+    collector
+        .fetch(SkillComponent.class)
+        .ifPresent(
+            (sc) ->
+                sc.addSkill(new IceWallSkill("IceWallSkill", 100, 3, Tuple.of(Resource.MANA, 0))));
+    return super.collect(itemEntity, collector);
+  }
+
+  @Override
+  public Optional<Entity> drop(final Point position) {
+    if (itemHolder != null) {
+      itemHolder
+          .fetch(IceMovementComponent.class)
+          .ifPresent((ic) -> itemHolder.remove(IceMovementComponent.class));
+      itemHolder.fetch(SkillComponent.class).ifPresent((sc) -> sc.removeSkill(IceWallSkill.class));
+      itemHolder = null;
+    }
+    return super.drop(position);
+  }
+}
